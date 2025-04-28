ef3c432c7d879a34379c4c203a6e572e0ebe0cce
CikeTheBear <dealmeidaromanelli@gmail.com>
2025-04-28 13:25:45 -0400

    Hello World exercises

--- CikeTheBear/HelloWorldExercises_CikeTheBear.java
+++ CikeTheBear/HelloWorldExercises_CikeTheBear.java
@@ -10,24 +10,50 @@ public class HelloWorldExercises_CikeTheBear {
     public static void main(String[] args) {
 
         // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+        System.out.println("Carlos De Almeida");
 
         // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+        System.out.println("Hola \nMundo");
 
         // 3. Añade un comentario sobre lo que hace cada línea del programa.
+        // No entiendo esta tarea
 
         // 4. Crea un comentario en varias líneas.
+        /**
+         *
+         *
+         * **/
 
         // 5. Imprime tu edad, tu color favorito y tu ciudad.
+        int edad = 33;
+        String color = "Azul";
+        String ciudad = "Caracas";
+
+        System.out.println("Tengo " + edad + ", mi color favorito es el " + color + " y vivo en " + ciudad);
 
         // 6. Explora los diferentes System.XXX.println(); más allá de "out".
+        System.err.println("Esto imprime un error");
+
 
         // 7. Utiliza varios println para imprimir una frase.
+        System.out.println("Esto");
+        System.out.println("es");
+        System.out.println("una");
+        System.out.println("frase");
+        System.out.println("en");
+        System.out.println("varias");
+        System.out.println("lineas");
 
         // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
+        System.out.println("X__X");
+        System.out.println("_  _");
+        System.out.println(" __ ");
 
         // 9. Intenta ejecutar el programa sin el método main y observa el error.
+        //Listo
 
         //10. Intenta cambiar el nombre del archivo a uno diferente del de la clase y compílalo. ¿Qué pasa?
+        //Listo
 
     }
 }
